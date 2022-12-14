c880cbddf5800637f587a7c428bfff074f28a3f1
Ra dar <wahradar511@gmail.com>
2022-12-14 16:01:47 +0700

    dashboard course

--- packages/webapp/src/routes/fronts/j_dashboardCourse.svelte
+++ packages/webapp/src/routes/fronts/j_dashboardCourse.svelte
@@ -10,8 +10,17 @@
         <!-- <img src="https://lambdasolusi.co.id/images/logo.PNG" alt="" class="h-20 w-20 flex justify-center mx-auto rounded-md my-4"> -->
         <div class="mx-6 mt-7">
             <h1 class="font-semibold text-sm mb-1">Java Fundamental</h1>
-            <p class="font-light text-sm  ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, possimus?</p>
-          </div>
+            <div class="flex mt-3">
+                <svg xmlns="http://www.w3.org/2000/svg" style="vertical-align: -0.125em;" width="18" height="18" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="#1b42fd" d="M14.55 16.55L11 13V8h2v4.175l2.95 2.95ZM11 6V4h2v2Zm7 7v-2h2v2Zm-7 7v-2h2v2Zm-7-7v-2h2v2Zm8 9q-2.075 0-3.9-.788q-1.825-.787-3.175-2.137q-1.35-1.35-2.137-3.175Q2 14.075 2 12t.788-3.9q.787-1.825 2.137-3.175q1.35-1.35 3.175-2.138Q9.925 2 12 2t3.9.787q1.825.788 3.175 2.138q1.35 1.35 2.137 3.175Q22 9.925 22 12t-.788 3.9q-.787 1.825-2.137 3.175q-1.35 1.35-3.175 2.137Q14.075 22 12 22Zm0-2q3.35 0 5.675-2.325Q20 15.35 20 12q0-3.35-2.325-5.675Q15.35 4 12 4Q8.65 4 6.325 6.325Q4 8.65 4 12q0 3.35 2.325 5.675Q8.65 20 12 20Zm0-8Z"/></svg>
+                <p class="font-light text-xs ml-1">40 Jam</p>
+              </div>
+
+              <div class="mt-3 flex">
+                <svg xmlns="http://www.w3.org/2000/svg" style="vertical-align: -0.125em;" width="18" height="18" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32"><path fill="currentColor" d="M7.92 2.656A2.224 2.224 0 0 1 10.067 1h3.082c1.237 0 2.089 1.164 1.792 2.319l-.002.006l-.178.675h8.8l.353-1.344A2.223 2.223 0 0 1 26.06 1h3.09c1.219 0 2.09 1.156 1.791 2.318l-.001.006l-6.857 26.032A2.22 2.22 0 0 1 21.94 31h-3.09c-1.237 0-2.087-1.164-1.79-2.318v-.006l.18-.676H8.434l-.353 1.34v.003A2.21 2.21 0 0 1 5.942 31H2.851c-1.237 0-2.089-1.163-1.792-2.319l.002-.006L7.92 2.655ZM18.029 25l1.054-4H10.28l-1.055 4h8.804Zm1.844-7l1.053-4h-8.801l-1.055 4h8.803Zm1.844-7l1.053-4h-8.8l-1.054 4h8.8Zm-8.76-8h-2.89a.224.224 0 0 0-.212.163L3.044 29h2.9a.21.21 0 0 0 .2-.159l.002-.006l.22-.835h-.82c-.365 0-.634-.371-.519-.742l.539-1.796A.632.632 0 0 1 6.17 25h.986l1.054-4H7.083c-.39 0-.677-.371-.554-.742l.575-1.796A.67.67 0 0 1 7.75 18h1.252l1.055-4H8.583c-.39 0-.677-.371-.554-.742l.575-1.796A.67.67 0 0 1 9.25 11h1.598l1.054-4h-1.319c-.39 0-.677-.37-.554-.74l.564-1.8A.67.67 0 0 1 11.24 4h1.453l.263-1Zm12.892.163L19.044 29h2.896c.091 0 .18-.056.214-.172L28.957 3H26.06a.224.224 0 0 0-.21.163Z"/></svg>
+                <p class="font-light text-xs ml-1">Pemula</p>
+            </div>
+            </div>
+          
           <!-- <div class="text-center border border-blue-500 px-1 mx-5 py-1 rounded  mt-7">
             <p class="text-xs">Finish : December 2022</>
             
